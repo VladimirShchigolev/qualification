67559db7018b0ad9b14f30631519564825f311c3
Krelkilet <vladimir_shchigolev@inbox.lv>
2022-01-09 21:08:08 +0200

    Check if database exists added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import os
 import sys
 
 from PySide6.QtGui import QFont
-from PySide6.QtWidgets import QApplication
-
+from PySide6.QtWidgets import QApplication, QMessageBox, QMainWindow
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -11,11 +11,23 @@ from src.widgets.main_window import MainWindow
 
 def main():
     """Starts the main window."""
+    app = QApplication([])
+
+    # check if database exists
+    if not os.path.isfile('configurations.db'):
+        window = QMainWindow()  # window needed for a message box
+        window.resize(1, 1)
+        window.show()
+        QMessageBox.critical(window,
+                             "Error", "Critical Error!\nFailed to open configurations database!",
+                             QMessageBox.Ok, QMessageBox.Ok)
+        window.close()
+        return
+
     # connect to the database
     engine = create_engine('sqlite:///configurations.db')
     session_cls = sessionmaker(bind=engine)
 
-    app = QApplication([])
     app.setFont(QFont("Lato", 12, QFont.Normal))
 
     window = MainWindow(session_cls)
